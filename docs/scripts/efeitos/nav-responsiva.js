466ecd567f7a6743b994823e80296cedeaa1143c
Alexandre Raminelli Junior <123258885+alexandreraminelli@users.noreply.github.com>
2023-06-18 13:38:52 -0300

    Menu responsivo para celulares

---
+++ docs/scripts/efeitos/nav-responsiva.js
@@ -0,0 +1,12 @@
+function navMobile() {
+  let botaoAbrir = document.querySelector("button#menu-mobile-abrir")
+  let menuMobile = document.querySelector("div#menu-mobile-aberto")
+
+  if (botaoAbrir.style.display != "none") {
+    botaoAbrir.style.display = "none"
+    menuMobile.style.display = "block"
+  } else {
+    botaoAbrir.style.display = "inline-block"
+    menuMobile.style.display = "none"
+  }
+}
